9298e938e8a00ace52e2540829780294815408e4
WidlerGXE <72944073+WidlerGXE@users.noreply.github.com>
2021-07-12 12:13:11 -0400

    Create _ide_helper_models.php

---
+++ _ide_helper_models.php
@@ -0,0 +1,78 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Relations
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $relation
+ * @property string $information
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $user2_id
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereInformation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereRelation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereUser2Id($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Relations whereUserId($value)
+ */
+	class Relations extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $two_factor_secret
+ * @property string|null $two_factor_recovery_codes
+ * @property string|null $remember_token
+ * @property int|null $current_team_id
+ * @property string|null $profile_photo_path
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read string $profile_photo_url
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCurrentTeamId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereProfilePhotoPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorRecoveryCodes($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereTwoFactorSecret($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
